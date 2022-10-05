db724272efe2a1b55cdbc07b8ed0a5b395b37c21
Madhurjya Boruah <67779864+madhurjyaboruah@users.noreply.github.com>
2022-10-05 23:05:30 +0530

    Update weather_map.py

--- weather_map.py
+++ weather_map.py
@@ -1,7 +1,7 @@
 from datetime import date, datetime
 import json,requests,sys
 from urllib import response
-APPID='fc56ca2f05fa1baa407711c533c0ea8d'
+APPID='add your own id'
 
 if len(sys.argv)<2:
     print('Usage: weather_map.py city_name 2-letter_country_code')
@@ -9,10 +9,9 @@ if len(sys.argv)<2:
 location=','.join(sys.argv[1:])
 print(location)
 
-#url ='http://api.openweathermap.org/data/2.5/weather?q=%s&dt=%d&appid=%s'% (location,dt,APPID)
+
 url=f'https://api.openweathermap.org/data/2.5/forecast?q={location}&cnt=5&appid={APPID}'
 print(url)
-#vrl='http://api.openweathermap.org/data/2.5/weather?q=Tezpur,IN&cnt=3&appid=fc56ca2f05fa1baa407711c533c0ea8d'
 
 response=requests.get(url)
 response.raise_for_status
@@ -48,16 +47,5 @@ else:
 
 
 
-# total arguments
-# n = len(sys.argv)
-# print("Total arguments passed:", n)
- 
-# # Arguments passed
-# print("\nName of Python script:", sys.argv[0])
- 
-# # print("\nArguments passed:", end = " ")
-# # for i in range(1, n):
-# #     print(sys.argv[i])
-# location=''.join(sys.argv[1:])
-# print(location+'qwq')
+
      
